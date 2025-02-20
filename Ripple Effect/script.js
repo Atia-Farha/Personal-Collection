c26f7574a78397e26e71641942667b51ad114a8e
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-02-20 20:19:11 +0600

    Update script.js

--- Ripple Effect/script.js
+++ Ripple Effect/script.js
@@ -1,8 +1,18 @@
-document.querySelector(".ripple-effect").onclick = function(e) {
+document.querySelector(".ripple-container").onclick = function (e) {
     let ripple = document.createElement("span");
     ripple.className = "ripple";
-    ripple.style.left = `${e.offsetX - 50}px`;
-    ripple.style.top = `${e.offsetY - 50}px`;
+
+    let rect = this.getBoundingClientRect();
+    let size = Math.max(rect.width, rect.height);
+
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+
+    ripple.style.width = ripple.style.height = `${size}px`;
+    ripple.style.left = `${x - size / 2}px`;
+    ripple.style.top = `${y - size / 2}px`;
+
     this.appendChild(ripple);
-    setTimeout(() => ripple.remove(), 500);
+
+    setTimeout(() => ripple.remove(), 600);
 };
